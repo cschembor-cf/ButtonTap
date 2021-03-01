d6d83558d25772f6b9368cf4172fdce9bfab17ed
Connor Schembor <connor.schembor@cardflight.com>
2021-03-01 11:01:29 -0500

    Initial commit

---
+++ ButtonTapper/ButtonTapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonTapperApp.swift
+//  ButtonTapper
+//
+//  Created by Connor Schembor on 3/1/21.
+//
+
+import SwiftUI
+
+@main
+struct ButtonTapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
